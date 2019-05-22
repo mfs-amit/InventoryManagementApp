13b27a4f08664fde1033122acc8326aed399ef42
Amit Soni <amitsonirsj@gmail.com>
2019-05-22 14:21:53 +0530

    task completed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true
 };
+
+export const baseUrl = 'http://localhost:3000/api/';
+export const apiUrls = {
+  login: baseUrl + 'users/login',
+  logout: baseUrl + 'users/logout',
+  products: baseUrl + 'products'
+}
