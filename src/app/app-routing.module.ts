0bc89cbd3c284e98462d093797cf4051142589b7
mfs-amit <amit.soni@mindfiresolutions.com>
2019-08-08 17:23:37 +0530

    updated register rounting guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "register", component: RegisterComponent },
+  { path: "register", component: RegisterComponent, canActivate: [LoginGuardGuard] },
   { path: "login", component: LoginComponent, canActivate: [LoginGuardGuard] },
   { path: "product", component: ProductComponent, canActivate: [AuthGuardGuard] },
   { path: "distributor", component: DistributorComponent, canActivate: [AuthGuardGuard] },
